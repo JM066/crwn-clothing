6c983e471b63603d3f6ce841653b77fc336c722d
mina <mina@dfrc.ch>
2021-06-19 00:30:25 +0900

    Shop and directory data moved to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,16 @@
-import React from 'react';
-
-import SHOP_DATA from './shop.data.js';
-
-import CollectionPreview from '../../components/collection-preview/collection-preview';
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default ShopPage;
+import React from "react";
+import { connect } from "react-redux";
+import CollectionPreview from "../../components/collection-preview/collection-preview";
+import { selectCollctions } from "../../redux/shop/shop.selectors";
+import { createStructuredSelector } from "reselect";
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollctions,
+});
+export default connect(mapStateToProps)(ShopPage);
