bd2c686e9244b5421ffeeed9fd054d523cb3f6b6
mina <mina@dfrc.ch>
2021-06-19 14:48:23 +0900

    Test

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,13 +3,17 @@ import { connect } from "react-redux";
 import CollectionPreview from "../../components/collection-preview/collection-preview";
 import { selectCollctions } from "../../redux/shop/shop.selectors";
 import { createStructuredSelector } from "reselect";
-const ShopPage = ({ collections }) => (
-  <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
-  </div>
-);
+const ShopPage = ({ collections }) => {
+  console.log("collections", collections);
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+
 const mapStateToProps = createStructuredSelector({
   collections: selectCollctions,
 });
